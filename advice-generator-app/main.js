2f16ceddaf2855cd73cba94e8779a54dfc8c9ba3
meowed <110143371+meow-ed@users.noreply.github.com>
2022-07-28 01:06:35 -0400

    add call to api and handle response

--- advice-generator-app/main.js
+++ advice-generator-app/main.js
@@ -1,1 +1,17 @@
+document.querySelector('button').addEventListener('click', updateQuote)
+
 const url = `https://api.adviceslip.com/advice`
+
+async function getQuote() {
+    const res = await fetch(url)
+    const obj = await res.json()
+    return obj.slip
+}
+
+async function updateQuote() {
+    const slip = await getQuote()
+    document.querySelector('#advice-id').textContent = `Advice #${slip.id}`
+    document.querySelector('#quotation').textContent = `${slip.advice}`
+}
+
+updateQuote()
